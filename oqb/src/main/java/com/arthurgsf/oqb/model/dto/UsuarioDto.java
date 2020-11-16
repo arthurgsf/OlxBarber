0e23ca862c6d05bbfd7435bd1b44bdd8e448930e
Arthur <arthurgsf@nca.ufma.br>
2020-11-16 16:42:23 -0300

    usuarioDto Criado

---
+++ oqb/src/main/java/com/arthurgsf/oqb/model/dto/UsuarioDto.java
@@ -0,0 +1,17 @@
+package com.arthurgsf.oqb.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UsuarioDto {
+    private String nome;
+    private String login;
+    private String senha;
+    private String email;
+    private String telefone;
+}
+
