7c6c913ce7341cb1ab0c172d1ae43df7308bb8ad
Thiago Wallass <thiagowallass@hotmail.com>
2020-11-18 17:17:12 -0300

    add quadra service

---
+++ oqb/src/main/java/com/arthurgsf/oqb/model/dto/QuadraDto.java
@@ -0,0 +1,24 @@
+package com.arthurgsf.oqb.model.dto;
+
+import com.arthurgsf.oqb.model.entity.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class QuadraDto {
+
+    private String nome;
+    private String endereco;
+    private String descricao;
+    private String esportes;
+    private Double preco;
+    private Timestamp horarios;
+    private Long idUsuario;
+
+}
